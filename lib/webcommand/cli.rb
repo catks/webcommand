1c06f057b74b2093891b6bbad140a51077d7728f
Carlos Atkinson <carlos.atks@gmail.com>
2019-10-29 22:15:12 -0300

    [PATCH] Update README
      * Updates README with basic usage instructions
      * Fix webcommand server
      * Adds new options to the webcommand server

--- lib/webcommand/cli.rb
+++ lib/webcommand/cli.rb
@@ -1,11 +1,12 @@
 module Webcommand
   class CLI < Thor
     desc "server", "Start the Webcommand server"
-    option :port, default: 9292
-    option :threads, default: 8
-    option :workers, default: 1
+    option :port, aliases: :p, default: 9292
+    option :bind, aliases: :b, default: '0.0.0.0'
+    option :threads, aliases: :t, default: 8
+    option :workers, aliases: :w, default: 1
     def server
-      exec "bundle exec puma " \
+      exec "puma " \
        "-p #{options[:port]} " \
        "-t #{options[:threads]}:#{options[:threads]} " \
        "-w #{options[:workers]} " \
